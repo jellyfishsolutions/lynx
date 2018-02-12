dd52af742ed1e1491f38b0a6c436d0b3ceda1894
Luca Roverelli <luca.roverelli@gmail.com>
2018-02-12 15:13:07 +0100

    Added documentation

--- lynx/base.middleware.ts
+++ lynx/base.middleware.ts
@@ -1,6 +1,10 @@
 import * as express from "express";
 import App from "./app";
 
+/**
+ * This class defines the base middleware class. Any middleware should be extends
+ * this abstract class, implementing the apply method.
+ */
 export abstract class BaseMiddleware {
     public app: App;
 
@@ -8,6 +12,11 @@ export abstract class BaseMiddleware {
         this.app = app;
     }
 
+    /**
+     * This method is automatically executed by the framework.
+     * @param req the standard express Request object
+     * @param res the standard express Response object
+     */
     abstract async apply(
         req: express.Request,
         res: express.Response
--- lynx/decorators.ts
+++ lynx/decorators.ts
@@ -23,6 +23,10 @@ export interface LynxControllerMetadata {
 
 let routes: LynxRouteMetadata[] = [];
 
+/**
+ * Decorator to set the base route of a controller.
+ * @param path the route path for the controller.
+ */
 export function Route(path: string) {
     return (target: any) => {
         target.controllerPath = path;
@@ -71,66 +75,116 @@ function setVerify(_: any, func: Function) {
     routes[routes.length - 1].verifiers.push(func);
 }
 
+/**
+ * Set the decorated method to a GET endpoint with the specified path.
+ * @param path the endpoint path
+ */
 export function GET(path: string) {
     return (target: any, method: any, _: any) => {
         setTarget(target, HttpVerb.GET, path, method);
     };
 }
 
+/**
+ * Set the decorated method to a POST endpoint with the specified path.
+ * @param path the endpoint path
+ */
 export function POST(path: string) {
     return (target: any, method: any, _: any) => {
         setTarget(target, HttpVerb.POST, path, method);
     };
 }
 
+/**
+ * Set the decorated method to a PUT endpoint with the specified path.
+ * @param path the endpoint path
+ */
 export function PUT(path: string) {
     return (target: any, method: any, _: any) => {
         setTarget(target, HttpVerb.PUT, path, method);
     };
 }
 
+/**
+ * Set the decorated method to a DELETE endpoint with the specified path.
+ * @param path the endpoint path
+ */
 export function DELETE(path: string) {
     return (target: any, method: any, _: any) => {
         setTarget(target, HttpVerb.DELETE, path, method);
     };
 }
 
+/**
+ * Set the decorated method to a PATH endpoint with the specified path.
+ * @param path the endpoint path
+ */
 export function PATCH(path: string) {
     return (target: any, method: any, _: any) => {
         setTarget(target, HttpVerb.PATCH, path, method);
     };
 }
 
+/**
+ * Add to the decorated method a body to be injected. The body will be validated
+ * using the specified schema.
+ * @param name the name of the argument in the method to map the body
+ * @param schema a JOI schema to validate the body object
+ */
 export function Body(name: string, schema: any) {
     return (target: any, _: any, __: any) => {
         setBody(target, name, schema);
     };
 }
 
+/**
+ * Set the decorated method to an API endpoints.
+ * In this way, the returned value of the method will be encapsulated in a
+ * standard API envelope and serialized to JSON using the Lynx serialization system.
+ */
 export function API() {
     return (target: any, _: any, __: any) => {
         setApi(target);
     };
 }
 
+/**
+ * Set the decorated method to accept MultipartForm requested.
+ */
 export function MultipartForm() {
     return (target: any, _: any, __: any) => {
         setMultipartForm(target);
     };
 }
 
+/**
+ * Add to the decorated method a route name, in order to easely generate redirect
+ * or, more general, the execution of the `route` method.
+ * @param name the name of the route
+ */
 export function Name(name: string) {
     return (_: any, __: any, ___: any) => {
         setName(name);
     };
 }
 
+/**
+ * Add to the decorated method a verification function that will be executed
+ * BEFORE the route. The function must NOT be an async function, and it shell
+ * return a boolean value. If true is returned, the method is then executed.
+ * This method is fundamental to implement authorization to a single endpoint.
+ * @param func the verification function to be executed. It must NOT be an async function, and return a boolean value.
+ */
 export function Verify(func: Function) {
     return (target: any, _: any, __: any) => {
         setVerify(target, func);
     };
 }
 
+/**
+ * Add to the decorated class a path to be executed as middleware.
+ * @param path the endpoint path
+ */
 export function Middleware(path: string) {
     return (target: any) => {
         target.middlewarePath = path;
